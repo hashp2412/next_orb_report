b8af718c4dba6b97d438fda5d785d399c5a0fa09
unknown <pharsh2412@gmail.com>
2024-01-06 16:50:43 +0530

    added tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      boxShadow: {
+        'navbar': '0px 2px 20px 0px rgba(0, 0, 0, 0.36)',
+        'text': '3px 8px 11px rgba(0, 0, 0, 0.12)',
+        'summary': '-8px -8px 12px 1px #21252B',
+        "s": '-10px -10px 12px #21252B, 10px 10px 12px rgba(0, 0, 0, 0.23);',
+      },
+      fontSize: {
+        '12xl': '12', 
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans'],
+        quick: ['Quicksand', 'sans'],
+        
+      },
+      letterSpacing: {
+        widest: '0.25rem',
+      },
+      borderWidth: {
+        'custom': '80%',
+      },
+      spacing: {
+        'custom-width': '200px',
+        'custom-height': '100px',
+        spacing: {
+        '20': '5rem', // For small screens
+        '50': '10rem',
+          'sf': '5rem', // For small screens
+          'mf': '10rem', // For large screens
+         // For large screens
+      },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
